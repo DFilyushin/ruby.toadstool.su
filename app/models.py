e2d743c32a3d7d02d8db7c21c1fab73965b094fa
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-10-17 14:35:23 +0700

    удалены неисп. файлы

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class DownloadStat(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    downloaded = db.Column(db.DateTime, index = True)
-    filename = db.Column(db.String(120))
-    ip = db.Column(db.String(20))
-
